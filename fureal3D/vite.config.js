8433f7fcf0d2c997596fb66055cdfb64bab269fc
phannguyenbuu <phannguyenbuu@gmail.com>
2025-10-30 07:51:01 +0700

    Full 3

--- fureal3D/vite.config.js
+++ fureal3D/vite.config.js
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    allowedHosts: 'all',
+    allowedHosts: ['n-lux.com', '.n-lux.com']
   },
 })
 
