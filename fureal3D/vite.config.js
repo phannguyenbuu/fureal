bb1a3bc37af2fccfdae53559980225f4435e855f
phannguyenbuu <phannguyenbuu@gmail.com>
2025-12-04 10:00:45 +0700

    M

--- fureal3D/vite.config.js
+++ fureal3D/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',  // ✅ BẮT BUỘC! Match nginx prefix
+  plugins: [react()], 
   server: {
     host: '0.0.0.0',
     port: 5173,
