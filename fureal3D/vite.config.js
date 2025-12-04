33c200aa69bb8dc75bc510528c9528fa2c4b2c63
phannguyenbuu <phannguyenbuu@gmail.com>
2025-12-04 12:36:43 +0700

    JKL

--- fureal3D/vite.config.js
+++ fureal3D/vite.config.js
@@ -2,14 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/fureal3D/' : '/',  // ✅ Dev '/', Prod '/fureal3D/'
+  base: mode === 'production' ? '/fureal3D/' : '/',
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true,     // Tốt nhất là true để tự động detect IP
     port: 5173,
-    hmr: {
-      host: '31.97.76.62',  // ✅ Server public IP cho HMR dev
-      port: 5173
-    }
+    hmr: true
   }
 }))
